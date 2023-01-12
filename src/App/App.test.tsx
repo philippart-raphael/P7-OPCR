d559ef2f4a9226b64d4293f9f36f9e11a476a284
philippart-raphael <raphael@neurokardiak.fr>
2023-01-12 06:25:47 +0100

    Correction App test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
+  const linkElement = screen.getByText(/Chez vous, partout et ailleurs/i);
   expect(linkElement).toBeInTheDocument();
 });
