d41a93efeac746bfbcb4c9316135fbb8c49cf9e6
philippart-raphael <raphael@neurokardiak.fr>
2023-01-12 06:53:55 +0100

    Refactoring

--- src/Component/Footer/Footer.tsx
+++ src/Component/Footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import './Footer.scss';
 
-export default function Footer({ img }: { img: string }) {
+export default function Footer({ img }: { img: string }): JSX.Element {
     return (
         <footer className="Footer">
-            <Logo img={img} altText="Logo Kasa" link={false} pathLink="/"  imgFilter="0%" imgWidth="100px"/>
+            <Logo img={img} altText="Logo Kasa" link={false} pathLink="/" imgFilter="0%" imgWidth="100px"/>
             <div>© Kasa, All rights reserved</div>
         </footer>
     );
