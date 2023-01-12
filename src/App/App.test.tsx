b012e969af74fbd7a2c3c74f3923274446b9cbc4
philippart-raphael <raphael@neurokardiak.fr>
2023-01-12 06:29:20 +0100

    Add Header test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Banner Text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Chez vous, partout et ailleurs/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Header Menu Text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Accueil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Header Menu Text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/À Propos/i);
   expect(linkElement).toBeInTheDocument();
 });
